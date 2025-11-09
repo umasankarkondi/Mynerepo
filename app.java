996ff39b1f3c01008d4e61fe608861ac4e3369d1
umasankarkondi <umasankarkondi945@gmail.com>
2025-11-09 10:32:29 +0530

    second code is added app java

--- app.java
+++ app.java
@@ -16,3 +16,24 @@ public class HelloWorld {
         System.out.println("You entered: " + number);
     }
 }
+
+//dev1 code added but not puules//
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  text-align: center;
+  color: red;
+} 
+</style>
+</head>
+<body>
+
+<p>Every paragraph will be affected by the style.</p>
+<p id="para1">Me too!</p>
+<p>And me!</p>
+
+</body>
+</html>
